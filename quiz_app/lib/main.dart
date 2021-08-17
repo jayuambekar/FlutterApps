5572a8949251892c60a63bea10257bece400a9de
Jayu Ambekar <jayu.ambekar@gmail.com>
2021-08-17 14:06:18 +0530

    changed quiz app

---
+++ quiz_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/splash.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Solve Quiz",
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: splashscreen(),
+    );
+  }
+}
