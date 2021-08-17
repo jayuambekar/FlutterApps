99b9a56e0565b2e506679cd2adc3d48d0fa22a79
Jayu Ambekar <jayu.ambekar@gmail.com>
2021-08-17 13:59:34 +0530

    Quiz app with mcq's, api

---
+++ quiz_app_multiple/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app_multiple/ui/pages/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Open Quiz',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.indigo,
+        fontFamily: "Montserrat",
+        buttonColor: Colors.pink,
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.pink,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          textTheme: ButtonTextTheme.primary
+        )
+      ),
+      home: HomePage(),
+    );
+  }
+}
